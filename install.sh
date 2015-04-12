173a03f5a09d32fd41a24e68c58b0262ff67272d
hooddanielc <hood.danielc@gmail.com>
2015-04-12 00:01:00 -0700

    fixes vimrc not working on ubuntu

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && ln ~/.vim/vimrc ~/.vimrc
